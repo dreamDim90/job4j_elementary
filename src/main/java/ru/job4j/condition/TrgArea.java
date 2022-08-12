c512c9a9b7f87760fcf1cb5c9629f6d6ccd8586c
mac <ideer@yandex.ru>
2022-08-12 18:38:49 +0500

    9. Площадь треугольника. [#175474]

--- src/main/java/ru/job4j/condition/TrgArea.java
+++ src/main/java/ru/job4j/condition/TrgArea.java
@@ -7,6 +7,6 @@ public class TrgArea {
 
     public static void main(String[] args) {
         double rsl = TrgArea.area(2, 2, 2);
-        System.out.println("area (2, 2, 2) = " + rsl);
+        System.out.println("area (9, 6, 9) = " + rsl);
     }
 }
