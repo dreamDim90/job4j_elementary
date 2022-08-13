120ae7227ac25f2e317b31b7678496a764a2812e
mac <ideer@yandex.ru>
2022-08-13 14:20:13 +0500

    0. Что такое тестирование [#159431 #331525]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -21,10 +21,10 @@ public class Converter {
         float out = Converter.rubleToEuro(in);
         boolean passed = expected == out;
         System.out.println("140 rubles are 2. Test result : " + passed);
-        float in1 = 120;
-        float expected1 = 2;
-        float out1 = Converter.rubleToDollar(in1);
-        boolean passed1 = expected1 == out1;
-        System.out.println("120 rubles are 2. Test result : " + passed1);
+        in = 120;
+        expected = 2;
+        out = Converter.rubleToDollar(in);
+        passed = expected == out;
+        System.out.println("120 rubles are 2. Test result : " + passed);
     }
 }
