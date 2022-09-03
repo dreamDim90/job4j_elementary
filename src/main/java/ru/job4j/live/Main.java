6394f8df55caa00e0647574991a423723fdef4fb
mac <ideer@yandex.ru>
2022-09-03 08:08:28 +0500

    5.9.1. Генерация конструкций в IDEA [#279134]

---
+++ src/main/java/ru/job4j/live/Main.java
@@ -0,0 +1,11 @@
+package ru.job4j.live;
+
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++) {
+            System.out.println("Hi Stas! Thanks for checking");
+        }
+    }
+}
